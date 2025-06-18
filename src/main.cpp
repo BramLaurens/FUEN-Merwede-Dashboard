d5a2e781d06613e55c4df13d207d4377d3baac2a
bramlaurens <bramlaurens@gmail.com>
2025-06-18 18:41:07 +0200

    Updated HTML handler for extra button

--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,27 @@
   Complete project details at https://randomnerdtutorials.com  
 *********/
 
-// Replace with your network credentials
+// Network credentials
 const char* ssid     = "FUEN-EV2A-1";
 const char* password = "123456789";
 
-// Set web server port number to 80
+// Server port
 WiFiServer server(80);
 
 // Variable to store the HTTP request
 String header;
 
-// Auxiliar variables to store the current output state
+// Buttonstates
 String output2State = "off";
 String output27State = "off";
+String runState = "Stopped";
 
 // Assign output variables to GPIO pins
-const int output2 = 2;
+const int output2 = 2; 
 const int output27 = 27;
+
+// Prototypes
+void HTML_handler();
 
 void setup() {
   Serial.begin(115200);
@@ -52,7 +56,11 @@ void setup() {
 }
 
 void loop() {
-  WiFiClient client = server.available();
+  HTML_handler();
+}
+
+void HTML_handler() {
+ WiFiClient client = server.available();
 
   if (client) {
     Serial.println("New Client.");
@@ -74,7 +82,17 @@ void loop() {
             } else if (header.indexOf("GET /2/off") >= 0) {
               output2State = "off";
               digitalWrite(output2, LOW);
-            } else if (header.indexOf("GET /27/on") >= 0) {
+            } 
+
+            else if (header.indexOf("GET /run_state/run") >= 0) {
+              runState = "Running";
+            }
+
+            else if (header.indexOf("GET /run_state/stop") >= 0) {
+             runState = "Stopped";
+            }
+
+            else if (header.indexOf("GET /27/on") >= 0) {
               output27State = "on";
               digitalWrite(output27, HIGH);
             } else if (header.indexOf("GET /27/off") >= 0) {
@@ -88,7 +106,14 @@ void loop() {
               client.println("Content-Type: application/json");
               client.println("Connection: close");
               client.println();
-              client.print("{\"2\":\"" + output2State + "\",\"27\":\"" + output27State + "\"}");
+
+              String json = "{";
+              json += "\"runState\":\"" + runState + "\",";
+              json += "\"2\":\"" + output2State + "\",";
+              json += "\"27\":\"" + output27State + "\"";
+              json += "}";
+
+              client.print(json);
             }
 
             // --- Serve style.css ---
