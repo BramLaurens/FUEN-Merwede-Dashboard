3f9894c0fa32222aa5fbfac38f34e5d2f32e6975
bramlaurens <bramlaurens@gmail.com>
2025-06-19 14:02:40 +0200

    Async integrated

--- src/main.cpp
+++ src/main.cpp
@@ -74,9 +74,8 @@ void setup() {
   // Remove the password parameter, if you want the AP (Access Point) to be open
   WiFi.softAP(ssid, password);
 
-  IPAddress IP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
-  Serial.println(IP);
+  Serial.println(WiFi.softAPIP());
   
   server.begin();
 
