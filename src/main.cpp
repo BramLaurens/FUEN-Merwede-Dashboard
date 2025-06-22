5804e3a4136f1215a8dc55a2322e5fe3052f95b8
bramlaurens <bramlaurens@gmail.com>
2025-06-22 18:55:53 +0200

    Also sending time over uart to display MCU

--- src/main.cpp
+++ src/main.cpp
@@ -388,6 +388,7 @@ void simSteps(){
     Serial.print("");
     Serial.println("Current load: " + String(usage_profile_base[currentHour]) + "W");
     displayUART('P', usage_profile_base[currentHour]);
+    displayUART('H', currentHour);
 
   }
 }
